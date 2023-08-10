6727f1399ea8bff4736c0faa52a532f8c9512e06
OddWays <95982923+oddways@users.noreply.github.com>
2023-08-10 22:37:58 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import WorkspaceComponent from "@/components/Workspace/WorkspaceComponent"
 
 export default function Home() {
   return (
-    <main className="w-full mt-2 min-h-[100vh] bg-gray-100 text-neutral-950 overflow-y-scroll">
+    <main className="w-full mt-2 min-h-screen bg-gray-100 text-neutral-950 overflow-y-scroll">
       <WorkspaceComponent />
     </main>
   )
